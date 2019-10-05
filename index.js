4b7fa8c5239f79ef92b3f73118f22a2fd78591e6
Theo <theodorejbrowne@gmail.com>
2019-10-05 14:15:36 -0700

    chords

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ const geo = require("d3-geomap")
 const selection = require("d3-selection")
 
 
-d3.select('body')
-    .append('svg')
-    .attr('width', 800)
-    .attr('height', 800)
- 
-    .attr('transform', 'translate(50,50)')
-
-const el = d3.select("svg")
- 
 const root = d3.hierarchy(parse(raw))
 // console.log(root)
 const nodes = d3.pack();
@@ -22,10 +13,10 @@ nodes.size([800, 800]);
 root.sum((el) => {
     return el.value;
 });
-
 nodes(root);
-// console.log(root.descendants())
-d3.select('svg')
+d3.select('.circles')
+    .append('svg')
+    .attr('transform', 'translate(50,50)')
     .attr('width', 800)
     .attr('height', 800)
     .selectAll('circle')
@@ -62,8 +53,6 @@ d3.select('svg')
         d3.select(this).attr("stroke", "rgb(0,0,0)")
         d3.select(this).attr("opacity", .5)
         // d3.select(this).attr("fill", "red")
-            
-       
         d3.select(this).append("text")
             .text(function (d) {
                 return d.children === undefined ? d.data.name : 'test';
@@ -74,7 +63,6 @@ d3.select('svg')
             .attr("fill", "black")
     })
     .on("mouseleave", function() {
-     
         d3.select(this).attr("stroke", null)
     })
     .on("click", function(d){
@@ -87,7 +75,11 @@ d3.select('svg')
     .attr('cx', function (d) { return d.x; })
     .attr('cy', function (d) { return d.y; })
     .attr('r', function (d) { return d.r; })
-
+    .append("text")
+    .text(function(d){
+        return "TEST"
+    })
+    
 let data = [
     [10, 20, 30],
     [40, 60, 80],
@@ -98,83 +90,17 @@ var links = correlation(data);
 var ribbonGenerator = d3.ribbon().radius(100);
 
    console.log(links)
-    let ele = d3.select(".info")
-    .attr('width', "20vw")
-    .attr('height', "20vh")
+    d3.select(".info")
+    .attr('width', "30vw")
+    .attr('height', "30vh")
     .append("svg")
-    .attr('width', "10vw")
-    .attr('height', "10vh")
-    // d3.select('')
+        .attr('width', "30vw")
+        .attr('height', "30vh")
+    .attr('transform', 'translate(50,50)')
     .selectAll('path')
     .data(links)
     .enter()
     .append('path')
     .attr('d', ribbonGenerator)
-    // .attr("margin-left", "100px")
-        .attr('cx', function (d) { 
-            // console.log(d)
-            return d.x; })
-        .attr('cy', function (d) { return d.y; })
-
-// let projection = d3.geoEqualEarth()
-// let path = d3.geoPath(projection)
-// let map = d3.map()
-// var projection = d3.geoAzimuthalEqualArea()
-//     .scale(300)
-//     .center([-3.0026, 16.7666])
-//     .translate([480, 250])
-// let world = d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json").then(res => {
-//     let countries = topo.feature(res, res.objects.countries)
-//     let path = d3.geoPath(projection)
-//     let projection = d3.geoEqualEarth()
-//     let map = new Map
-//     d3.select(".map")
-//     .attr('width', "20vw")
-//     .attr('height', "20vh")
-//     .append("svg")
-//     .attr('width', "10vw")
-//     .attr('height', "10vh")
-//     .selectAll("path")
-//     .data(countries.features)
-//     .join("path")
-// })
-// let countries = topo.feature(world, world.objects.countries)
-// debugger
-// let path = d3.geoPath(projection)
-// let projection = d3.geoEqualEarth()
-// let map = new Map
-
-// d3.select(".map")
-//     .attr('width', "20vw")
-//     .attr('height', "20vh")
-//     .append("svg")
-//     .attr('width', "10vw")
-//     .attr('height', "10vh")
-//     .selectAll("path")
-//     .data(countries.features)
-//     .join("path")
-        
-
-
-// const worldMap = geo.geomap();
-// worldMap.geofile('./node_modules/d3-geomap/src/world/countries.json');
-// // worldMap.draw()
-// worldMap.draw(selection.select('#map'))
-
-// var format = function (d) {
-//     d = d / 1000000;
-//     return d3.format(',.02f')(d) + 'M';
-// }
-
-// var map = d3.choropleth()
-//     .geofile('/d3-geomap/topojson/world/countries.json')
-//     .colors(d3.schemeYlGnBu[9])
-//     .column('YR2010')
-//     .format(format)
-//     .legend(true)
-//     .unitId('iso3');
-
-// d3.csv('/data/sp.pop.totl.csv').then(data => {
-//     var selection = d3.select('#map').datum(data);
-//     map.draw(selection);
-// });
+    .attr('transform', 'translate(150,150)')
+    .attr("fill", "blue")
--- mapping.js
+++ mapping.js
@@ -61,3 +61,108 @@
 //     var selection = d3.select('#map').datum(data);
 //     map.draw(selection);
 // });
+
+
+
+// var projection = d3.geoEquirectangular();
+
+// var geoGenerator = d3.geoPath()
+//     .projection(projection);
+// let map  = geoGenerator('./countries.geojson')
+
+//      d3.select('#map')
+//     .append("svg")
+//     .selectAll("path")
+//     .data(map)
+//     .enter()
+//     .append('path')
+
+d3.json('https://datahub.io/core/geo-countries/datapackage.json').then(data => {
+
+    // let map = d3.map(data)
+    // d3.select('#map')
+    // .append("svg")
+    // .selectAll("path")
+    // .data(map);
+
+    // map.draw(selection);
+});
+
+// let projection = d3.geoEqualEarth()
+// let path = d3.geoPath(projection)
+// let map = d3.map()
+// var projection = d3.geoAzimuthalEqualArea()
+//     .scale(300)
+//     .center([-3.0026, 16.7666])
+//     .translate([480, 250])
+// let world = d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json").then(res => {
+//     let countries = topo.feature(res, res.objects.countries)
+//     let path = d3.geoPath(projection)
+//     let projection = d3.geoEqualEarth()
+//     let map = new Map
+//     d3.select(".map")
+//     .attr('width', "20vw")
+//     .attr('height', "20vh")
+//     .append("svg")
+//     .attr('width', "10vw")
+//     .attr('height', "10vh")
+//     .selectAll("path")
+//     .data(countries.features)
+//     .join("path")
+// })
+// let countries = topo.feature(world, world.objects.countries)
+// debugger
+// let path = d3.geoPath(projection)
+// let projection = d3.geoEqualEarth()
+// let map = new Map
+
+// d3.select(".map")
+//     .attr('width', "20vw")
+//     .attr('height', "20vh")
+//     .append("svg")
+//     .attr('width', "10vw")
+//     .attr('height', "10vh")
+//     .selectAll("path")
+//     .data(countries.features)
+//     .join("path")
+
+
+
+// const worldMap = geo.geomap();
+// worldMap.geofile('./node_modules/d3-geomap/src/world/countries.json');
+// // worldMap.draw()
+// worldMap.draw(selection.select('#map'))
+
+var format = function (d) {
+    d = d / 1000000;
+    return d3.format(',.02f')(d) + 'M';
+}
+
+let mappy = geo.choropleth()
+    .geofile('https://datahub.io/core/geo-countries/datapackage.json')
+    .colors(d3.schemeYlGnBu[9])
+    .column('YR2010')
+    .format(format)
+    .legend(true)
+    .unitId('iso3');
+var selecty = d3.select('#map').datum(data);
+mappy.draw(selecty);
+
+// d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json').then(data => {
+//     var selection = d3.select('#map').datum(data);
+//     mappy.draw(selection);
+// });
+// const worldMap = geo.geomap();
+// worldMap.geofile('https://gist.github.com/markmarkoh/2969317');
+
+// worldMap.draw(d3.select('#map'));
+// var projection = d3
+//     .geoEquirectangular()
+//     .center([0, 15]) // set centre to further North
+//     .scale([1000 / (2 * Math.PI)]) // scale to fit group width
+//     .translate([1000 / 2,1000 / 2]) // ensure centred in group
+//     ;
+// var path = d3
+//     .geoPath()
+//     .projection(projection)
+//     ;
